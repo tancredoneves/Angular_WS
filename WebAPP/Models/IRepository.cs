ae3b81fab3495bc20a9e3f1eb879933876e769f7
tancredofernando <tancredofernandoneves@gmail.com>
2017-03-20 20:31:55 -0300

    Primeiro commit

---
+++ WebAPP/Models/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace WebAPP.Models
+{
+    public interface IRepository
+    {
+        IQueryable<Order> GetAllOrdersWithDetails();
+        Order GetOrder(int id);
+        IQueryable<Order> GettAllOrders();
+    }
+}
---
+++ WebAPP/Models/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPP.Models
+{
+    public class Repository : IRepository
+    {
+        private WebAPPContext db;
+
+        public Repository(WebAPPContext db)
+        {
+            this.db = db;
+        }
+
+        public IQueryable<Order> GettAllOrders()
+        {
+            return db.Orders;
+        }
+
+        public IQueryable<Order> GetAllOrdersWithDetails()
+        {
+            return db.Orders.Include("OrderDetails");
+        }
+
+        public Order GetOrder(int id)
+        {
+            return db.Orders.Include("OrderDetails.Book").
+                FirstOrDefault(o => o.Id == id);
+        }
+
+    }
+}
